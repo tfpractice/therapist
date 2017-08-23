85139fa4fe44bde720a4f94c22afa9d1012fc2fd
theory friction practice <jackson.brebnor@gmail.com>
2017-08-22 21:51:07 -0400

    about page

--- src/components/about/main.js
+++ src/components/about/main.js
@@ -3,36 +3,40 @@ import Grid from 'material-ui/Grid';
 import Text from 'material-ui/Typography';
 import { MarkdownPreview } from 'react-marked-markdown';
 import withStyles from 'material-ui/styles/withStyles';
-import Card, { CardMedia, CardContent } from 'material-ui/Card';
+import Card, { CardMedia, CardHeader, CardContent } from 'material-ui/Card';
 
 import { mkOpts } from '../../utils';
 import { bio, quote } from './about';
 
+const sheet = { name: 'About' };
 const styles = theme => ({ image: { maxWidth: '90%' }});
-const Styled = withStyles(styles);
-const About = ({ classes }) =>
-  (<Grid container justify="center" align="center">
-    <Grid item xs={9}>
-      <Card>
-        <CardContent>
-          <Text color="secondary">
-            <MarkdownPreview value={quote} {...mkOpts} />
-          </Text>
-        </CardContent>
-        <CardContent>
-          <Grid container justify="center" align="center">
-            <Grid item xs={6} sm={4}>
-              <img className={classes.image} src="/images/headshot.jpg" />
+const Styled = withStyles(styles, sheet);
+
+const About = ({ classes }) => {
+  console.log('classes', classes);
+  return (
+    <Grid container justify="center" align="center">
+      <Grid item xs={9}>
+        <Card>
+          <CardHeader
+            subheader={<MarkdownPreview value={quote} {...mkOpts} />}
+          />
+          <CardContent>
+            <Grid container justify="center" align="center">
+              <Grid item xs={11} sm={4}>
+                <img className={classes.image} src="/images/headshot.jpg" />
+              </Grid>
+              <Grid item xs={11} sm>
+                <Text component={CardContent} color="secondary">
+                  <MarkdownPreview value={bio} {...mkOpts} />
+                </Text>
+              </Grid>
             </Grid>
-            <Grid item xs={6} sm>
-              <Text color="secondary">
-                <MarkdownPreview value={bio} {...mkOpts} />
-              </Text>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>}
-  </Grid>);
+          </CardContent>
+        </Card>
+      </Grid>}
+    </Grid>
+  );
+};
 
 export default Styled(About);
