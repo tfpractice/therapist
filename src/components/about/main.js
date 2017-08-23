45b17fac59c92f671120e6158962bff16a5d0aab
theory friction practice <jackson.brebnor@gmail.com>
2017-08-22 21:52:59 -0400

    about media

--- src/components/about/main.js
+++ src/components/about/main.js
@@ -8,35 +8,29 @@ import Card, { CardMedia, CardHeader, CardContent } from 'material-ui/Card';
 import { mkOpts } from '../../utils';
 import { bio, quote } from './about';
 
-const sheet = { name: 'About' };
 const styles = theme => ({ image: { maxWidth: '90%' }});
+const sheet = { name: 'About' };
 const Styled = withStyles(styles, sheet);
 
-const About = ({ classes }) => {
-  console.log('classes', classes);
-  return (
-    <Grid container justify="center" align="center">
-      <Grid item xs={9}>
-        <Card>
-          <CardHeader
-            subheader={<MarkdownPreview value={quote} {...mkOpts} />}
-          />
-          <CardContent>
-            <Grid container justify="center" align="center">
-              <Grid item xs={11} sm={4}>
-                <img className={classes.image} src="/images/headshot.jpg" />
-              </Grid>
-              <Grid item xs={11} sm>
-                <Text component={CardContent} color="secondary">
-                  <MarkdownPreview value={bio} {...mkOpts} />
-                </Text>
-              </Grid>
+const About = ({ classes }) =>
+  (<Grid container justify="center" align="center">
+    <Grid item xs={9}>
+      <Card>
+        <CardHeader subheader={<MarkdownPreview value={quote} {...mkOpts} />} />
+        <CardContent>
+          <Grid container justify="center" align="center">
+            <Grid item xs={11} sm={4}>
+              <img className={classes.image} src="/images/headshot.jpg" />
             </Grid>
-          </CardContent>
-        </Card>
-      </Grid>}
-    </Grid>
-  );
-};
+            <Grid item xs={11} sm>
+              <Text component={CardContent} color="secondary">
+                <MarkdownPreview value={bio} {...mkOpts} />
+              </Text>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>}
+  </Grid>);
 
 export default Styled(About);
