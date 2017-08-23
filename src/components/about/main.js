34e01cbebf6aba5bc64cb3c4df0ed117bb23432c
theory friction practice <jackson.brebnor@gmail.com>
2017-08-22 21:43:55 -0400

    marked utils

--- src/components/about/main.js
+++ src/components/about/main.js
@@ -5,45 +5,34 @@ import { MarkdownPreview } from 'react-marked-markdown';
 import withStyles from 'material-ui/styles/withStyles';
 import Card, { CardMedia, CardContent } from 'material-ui/Card';
 
-// import { mkOpts } from '../../utils';
+import { mkOpts } from '../../utils';
 import { bio, quote } from './about';
-
-const mkOpts = {
-  markedOptions: {
-    sanitize: false,
-    smartypants: true,
-  },
-};
 
 const styles = theme => ({ image: { maxWidth: '90%' }});
 const Styled = withStyles(styles);
-const About = ({ classes }) => {
-  console.log('classes', classes);
-  return (
-    <Grid container justify="center" align="center">
-      <Grid item xs={9}>
-        <Card>
-          <CardContent>
-            <Text color="secondary">
-              <MarkdownPreview value={quote} {...mkOpts} />
-            </Text>
-          </CardContent>
-          <CardContent>
-            <Grid container justify="center" align="center">
-              <Grid item xs={6} sm={4}>
-                <img className={classes.image} src="/images/headshot.jpg" />
-              </Grid>
-              <Grid item xs={6} sm>
-                <Text color="secondary">
-                  <MarkdownPreview value={bio} {...mkOpts} />
-                </Text>
-              </Grid>
+const About = ({ classes }) =>
+  (<Grid container justify="center" align="center">
+    <Grid item xs={9}>
+      <Card>
+        <CardContent>
+          <Text color="secondary">
+            <MarkdownPreview value={quote} {...mkOpts} />
+          </Text>
+        </CardContent>
+        <CardContent>
+          <Grid container justify="center" align="center">
+            <Grid item xs={6} sm={4}>
+              <img className={classes.image} src="/images/headshot.jpg" />
             </Grid>
-          </CardContent>
-        </Card>
-      </Grid>}
-    </Grid>
-  );
-};
+            <Grid item xs={6} sm>
+              <Text color="secondary">
+                <MarkdownPreview value={bio} {...mkOpts} />
+              </Text>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>}
+  </Grid>);
 
 export default Styled(About);
--- src/utils/marked.js
+++ src/utils/marked.js
@@ -1,15 +1,6 @@
-// import React from 'read';
-// import { MarkdownPreview } from 'react-marked-markdown';
-// import withStyles from 'material-ui/styles/withStyles';
-// import Card, { CardMedia, CardContent } from 'material-ui/Card';
-//
-// import { bio, quote } from './about';
-
-const mkOpts = {
+export const mkOpts = {
   markedOptions: {
     sanitize: false,
     smartypants: true,
   },
 };
-
-export { mkOpts };
