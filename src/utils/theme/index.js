ce0f93a53e0e9c2a42cff8adfe462af79af9ebba
tfpractice <jackson.brebnor@gmail.com>
2018-03-13 17:53:12 -0400

    muib16

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -1,12 +1,15 @@
 import { createMuiTheme } from 'material-ui/styles/';
-import { cyan, blueGrey, grey } from 'material-ui/colors';
+import { blueGrey, grey } from 'material-ui/colors';
+import { white } from 'material-ui/colors/common';
 
 const primary = blueGrey;
-const accent = grey;
+const accent = white;
+const secondary = grey;
 const type = 'dark';
 
 const palette = {
   primary,
+  secondary,
   accent,
   type,
 };
